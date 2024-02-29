950a7a99a996fe6740b1c9f2ad06a85415a42033
Sheldon Liao <sheldon.liao1015@gmail.com>
2024-02-29 10:28:58 +0800

    feat: 模糊搜索和结果匹配高亮

--- src/layouts/_common/search-bar.tsx
+++ src/layouts/_common/search-bar.tsx
@@ -1,20 +1,42 @@
 import { IconButton, SvgIcon } from '@/components/icon';
 import { useFlattenedRoutes } from '@/router/hooks';
 import { Empty, Input, InputRef, Modal, Tag } from 'antd';
-import { useState, useRef } from 'react';
+import { useState, useRef, CSSProperties, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import parse from 'autosuggest-highlight/parse';
 import match from 'autosuggest-highlight/match';
 import Scrollbar from '@/components/scrollbar';
+import { useThemeToken } from '@/theme/hooks';
+import Color from 'color';
 
 export default function SearchBar() {
   const { t } = useTranslation();
   const flattenedRoutes = useFlattenedRoutes();
+  const themeToken = useThemeToken();
 
+  // ref
+  const inputRef = useRef<InputRef>(null);
+  // state
   const [search, setSearch] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResult, setSearchResult] = useState(flattenedRoutes);
-  const inputRef = useRef<InputRef>(null);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
+
+  const activeStyle: CSSProperties = {
+    border: `1px dashed ${themeToken.colorPrimary}`,
+    backgroundColor: `${Color(themeToken.colorPrimary).alpha(0.2).toString()}`,
+  };
+
+  //刷选搜索结果
+  useEffect(() => {
+    const result = flattenedRoutes.filter(
+      (item) =>
+        t(item.label).toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1 ||
+        t(item.key).toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
+    );
+    setSearchResult(result);
+    setSelectedItemIndex(0);
+  }, [searchQuery, flattenedRoutes, t]);
 
   const handleOpen = () => {
     setSearch(true);
@@ -31,13 +53,24 @@ export default function SearchBar() {
     }
   };
 
+  const handleHover = (index: number) => {
+    if (index === selectedItemIndex) return;
+    setSelectedItemIndex(index);
+  };
+
+  const handleSelect = (key: string) => {
+    //TODO: 完成所有页面后再实现
+    // replace(key);
+    // handleCancel();
+  };
+
   return (
     <>
       <div className="flex items-center justify-center">
         <IconButton className="h-10 w-10" onClick={handleOpen}>
           <SvgIcon icon="ic-search" size="20" />
         </IconButton>
-        <IconButton className="h-6 rounded-md bg-[#63738114] text-xs font-bold">
+        <IconButton className="h-6 rounded-md bg-hover text-xs font-bold">
           ⌘K
         </IconButton>
       </div>
@@ -95,17 +128,40 @@ export default function SearchBar() {
                 );
                 const partsKey = parse(t(key), match(t(key), searchQuery));
                 return (
-                  <div className="flex flex-col cursor-pointer w-full px-4 py-2 rounded-lg border-dashed border-b-indigo-500">
+                  <div
+                    className="flex flex-col cursor-pointer w-full px-4 py-2 rounded-lg border-dashed border-b-indigo-500"
+                    style={index === selectedItemIndex ? activeStyle : {}}
+                    onClick={() => handleSelect(key)}
+                    onMouseMove={() => handleHover(index)}
+                  >
                     <div className="flex justify-between">
                       <div className="">
                         <div className="font-medium">
                           {partsTitle.map((item) => (
-                            <span key={item.text}>{item.text}</span>
+                            <span
+                              key={item.text}
+                              style={{
+                                color: item.highlight
+                                  ? themeToken.colorPrimary
+                                  : themeToken.colorText,
+                              }}
+                            >
+                              {item.text}
+                            </span>
                           ))}
                         </div>
                         <div className="text-xs">
                           {partsKey.map((item) => (
-                            <span key={item.text}>{item.text}</span>
+                            <span
+                              key={item.text}
+                              style={{
+                                color: item.highlight
+                                  ? themeToken.colorPrimary
+                                  : themeToken.colorTextDescription,
+                              }}
+                            >
+                              {item.text}
+                            </span>
                           ))}
                         </div>
                       </div>
