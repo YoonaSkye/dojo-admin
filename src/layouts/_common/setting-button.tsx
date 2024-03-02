6168498697a35748d92930b1508d75cbd7e79f2b
Sheldon Liao <sheldon.liao1015@gmail.com>
2024-03-02 13:36:16 +0800

    feat: setting button theme mode function

--- src/layouts/_common/setting-button.tsx
+++ src/layouts/_common/setting-button.tsx
@@ -1,10 +1,24 @@
 import { IconButton, SvgIcon } from '@/components/icon';
-import { Button, Drawer } from 'antd';
+import { Button, Card, Drawer } from 'antd';
 import { useState } from 'react';
 import { CloseOutlined } from '@ant-design/icons';
+import { useThemeToken } from '@/theme/hooks';
+import { useSettingActions, useSettings } from '@/store/settingStore';
+import { ThemeMode } from '#/enum';
 
 export default function SettingButton() {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const { colorTextSecondary, colorPrimary } = useThemeToken();
+  const settings = useSettings();
+  const { themeMode } = settings;
+  const { setSettings } = useSettingActions();
+
+  const setThemeMode = (themeMode: ThemeMode) => {
+    setSettings({
+      ...settings,
+      themeMode,
+    });
+  };
 
   return (
     <>
@@ -20,6 +34,7 @@ export default function SettingButton() {
         onClose={() => setDrawerOpen(false)}
         open={drawerOpen}
         closeIcon={false}
+        bodyStyle={{ padding: 0 }}
         extra={
           <IconButton
             onClick={() => setDrawerOpen(false)}
@@ -32,6 +47,36 @@ export default function SettingButton() {
       >
         <div className="flex flex-col gap-6 p-6">
           {/* theme mode */}
+          <div className="">
+            <div
+              className="mb-3 text-base font-semibold"
+              style={{ color: colorTextSecondary }}
+            >
+              Mode
+            </div>
+            <div className="flex flex-row gap-4">
+              <Card
+                className="flex items-center justify-center h-20 w-full cursor-pointer"
+                onClick={() => setThemeMode(ThemeMode.Light)}
+              >
+                <SvgIcon
+                  icon="ic-settings-mode-sun"
+                  size="24"
+                  color={themeMode === ThemeMode.Light ? colorPrimary : ''}
+                />
+              </Card>
+              <Card
+                className="flex items-center justify-center h-20 w-full cursor-pointer"
+                onClick={() => setThemeMode(ThemeMode.Dark)}
+              >
+                <SvgIcon
+                  icon="ic-settings-mode-moon"
+                  size="24"
+                  color={themeMode === ThemeMode.Dark ? colorPrimary : ''}
+                />
+              </Card>
+            </div>
+          </div>
 
           {/* theme layout */}
 
