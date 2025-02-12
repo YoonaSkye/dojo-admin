41e122de597c2259e4e66f05626276354a55791a
Sheldon Liao <sheldon.liao1015@gmail.com>
2025-02-12 22:16:26 +0800

    refactor: 更新utils目录

---
+++ src/utils/route.ts
@@ -0,0 +1,30 @@
+import { AppRouteObject } from '#/router';
+
+/**
+ *
+ * @param absolutePath 当前location中的路由地址
+ * @param routes 全部权限路由
+ * @returns 匹配的路由对象
+ */
+const findRouteByAbsolutePath = (
+  absolutePath: string,
+  routes: AppRouteObject[]
+) => {
+  function find(routes: Array<AppRouteObject>): AppRouteObject | undefined {
+    for (let i = 0; i < routes.length; i++) {
+      const route = routes[i];
+      if (route.path === absolutePath) {
+        return route;
+      }
+      if (route.children && route.children.length > 0) {
+        const targetRoute = find(route.children);
+        if (targetRoute) {
+          return targetRoute;
+        }
+      }
+    }
+  }
+  return find(routes);
+};
+
+export { findRouteByAbsolutePath };
--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -5,6 +5,43 @@ interface TreeConfigOptions {
 }
 
 /**
+ * @zh_CN 遍历树形结构，并返回所有节点中指定的值。
+ * @param tree 树形结构数组
+ * @param getValue 获取节点值的函数
+ * @param options 作为子节点数组的可选属性名称。
+ * @returns 所有节点中指定的值的数组
+ */
+export function traverseTreeValues<T, V>(
+  tree: T[],
+  getValue: (node: T) => V,
+  options?: TreeConfigOptions
+): V[] {
+  const result: V[] = [];
+  const { childProps } = options || {
+    childProps: 'children',
+  };
+
+  const dfs = (treeNode: T) => {
+    const value = getValue(treeNode);
+    result.push(value);
+    const children = (treeNode as Record<string, any>)?.[childProps];
+    if (!children) {
+      return;
+    }
+    if (children.length > 0) {
+      for (const child of children) {
+        dfs(child);
+      }
+    }
+  };
+
+  for (const treeNode of tree) {
+    dfs(treeNode);
+  }
+  return result.filter(Boolean);
+}
+
+/**
  * Flatten an array containing a tree structure
  * @param {T[]} trees - An array containg a tree structure
  * @returns {T[]} - Flattened array
@@ -39,4 +76,35 @@ export function mapTree<T, V extends Record<string, any>>(
     }
     return mapperNode as V;
   });
+}
+
+/**
+ * 根据条件过滤给定树结构的节点，并以原有顺序返回所有匹配节点的数组。
+ * @param tree 要过滤的树结构的根节点数组。
+ * @param filter 用于匹配每个节点的条件。
+ * @param options 作为子节点数组的可选属性名称。
+ * @returns 包含所有匹配节点的数组。
+ */
+export function filterTree<T extends Record<string, any>>(
+  tree: T[],
+  filter: (node: T) => boolean,
+  options?: TreeConfigOptions
+): T[] {
+  const { childProps } = options || {
+    childProps: 'children',
+  };
+
+  const _filterTree = (nodes: T[]): T[] => {
+    return nodes.filter((node: Record<string, any>) => {
+      if (filter(node as T)) {
+        if (node[childProps]) {
+          node[childProps] = _filterTree(node[childProps]);
+        }
+        return true;
+      }
+      return false;
+    });
+  };
+
+  return _filterTree(tree);
 }
