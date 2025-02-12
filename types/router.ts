112371affb65361a2d29d98c6117c50dda15f4df
Sheldon Liao <sheldon.liao1015@gmail.com>
2025-02-12 22:20:55 +0800

    refactor: 更新RouteMeta类型定义

--- types/router.ts
+++ types/router.ts
@@ -1,57 +1,133 @@
-import { Params, RouteObject } from 'react-router-dom';
+import { RouteObject } from 'react-router-dom';
 
 // 扩展meta元信息接口
 export interface RouteMeta {
   /**
-   * antd menu selectedKeys
+   * 激活图标（菜单）
    */
-  key: string;
+  activeIcon?: string;
   /**
-   * menu label, i18n
+   * 当前激活的菜单，有时候不想激活现有菜单，需要激活父级菜单时使用
    */
-  label: string;
+  activePath?: string;
   /**
-   * menu prefix icon
+   * 是否固定标签页
+   * @default false
+   */
+  affixTab?: boolean;
+  /**
+   * 固定标签页的顺序
+   * @default 0
+   */
+  affixTabOrder?: number;
+  /**
+   * 需要特定的角色标识才可以访问
+   * @default []
+   */
+  authority?: string[];
+  /**
+   * 徽标
+   */
+  badge?: string;
+  /**
+   * 徽标类型
+   */
+  badgeType?: 'dot' | 'normal';
+  /**
+   * 徽标颜色
+   */
+  badgeVariants?:
+    | 'default'
+    | 'destructive'
+    | 'primary'
+    | 'success'
+    | 'warning'
+    | string;
+  /**
+   * 当前路由的子级在菜单中不展现
+   * @default false
+   */
+  hideChildrenInMenu?: boolean;
+  /**
+   * 当前路由在面包屑中不展现
+   * @default false
+   */
+  hideInBreadcrumb?: boolean;
+  /**
+   * 当前路由在菜单中不展现
+   * @default false
+   */
+  hideInMenu?: boolean;
+  /**
+   * 当前路由在标签页不展现
+   * @default false
+   */
+  hideInTab?: boolean;
+  /**
+   * 图标（菜单/tab）
    */
   icon?: string;
   /**
-   * menu suffix icon
+   * iframe 地址
    */
-  suffix?: React.ReactNode;
+  iframeSrc?: string;
   /**
-   * hide in menu
+   * 忽略权限，直接可以访问
+   * @default false
    */
+  ignoreAccess?: boolean;
+  /**
+   * 开启KeepAlive缓存
+   */
+  keepAlive?: boolean;
+  /**
+   * 外链-跳转路径
+   */
+  link?: string;
+  /**
+   * 路由是否已经加载过
+   */
+  loaded?: boolean;
+  /**
+   * 标签页最大打开数量
+   * @default false
+   */
+  maxNumOfOpenTab?: number;
+  /**
+   * 菜单可以看到，但是访问会被重定向到403
+   */
+  menuVisibleWithForbidden?: boolean;
+  /**
+   * 当前路由不使用基础布局（仅在顶级生效）
+   */
+  noBasicLayout?: boolean;
+  /**
+   * 在新窗口打开
+   */
+  openInNewWindow?: boolean;
+  /**
+   * 用于路由->菜单排序
+   */
+  order?: number;
+  /**
+   * 菜单所携带的参数
+   */
+  query?: Recordable;
+  /**
+   * 标题名称
+   */
+  title?: string;
+  label?: string;
+  key?: string;
   hideMenu?: boolean;
-  /**
-   * hide in multi tab
-   */
   hideTab?: boolean;
-  /**
-   * disable in menu
-   */
   disabled?: boolean;
-  /**
-   * react router outlet
-   */
-  outlet?: any;
-  /**
-   * use to refresh tab
-   */
-  timeStamp?: string;
-  /**
-   * external link and iframe need
-   */
-  // frameSrc?: string;
-  /**
-   * dynamic route params
-   *
-   * @example /user/:id
-   */
-  params?: Params<string>;
+  name?: string;
 }
 
 export type AppRouteObject = {
-  order?: number;
+  redirect?: string;
   meta?: RouteMeta;
+  handle?: RouteMeta;
   children?: AppRouteObject[];
 } & Omit<RouteObject, 'children'>;
